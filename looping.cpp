f2ff1aab8b964351fae1407ab0ddf54096bbece3
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-30 18:32:34 +0700

    menambahkan logika not

--- looping.cpp
+++ looping.cpp
@@ -39,3 +39,26 @@ int main() {
     cout << "Bilangan acak do-while yang terakhir =" <<x <<endl;
     cout<<endl;
 }
+
+Logika not
+#include <iostream>
+using namespace std;
+
+int main() {
+    float nilB,nilM,rerata;
+    string status;
+
+    cout <<"Masukkan nilai Matematika = ";
+    cin >>nilM;
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout <<"Status kelulusan = "<<status<<",dengan nilai rata-rata = " <<rerata<<endl;
+}
