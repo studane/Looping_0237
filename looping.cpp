93981e79f2f1af51f0420893258187f3a448501d
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-30 18:27:07 +0700

    menambah perulangan do while

--- looping.cpp
+++ looping.cpp
@@ -7,23 +7,35 @@ int main() {
 
     // Perulangan FOR
     // mencetak tulisan sebanyak 5 kali
-    cout << "PERULANGAN FOR" << endl;
+    cout <<"PERULANGAN FOR" <<endl;
     for (int i = 0; i <= 4; i++) {
-        cout << "Teknologi Informasi UMY" << endl;
+        cout <<"Teknologi Informasi UMY" <<endl;
     }
-    cout << endl;
+    cout <<endl;
     //untuk mendapatkan urutan angka acak yang berbeda
     srand(time(0));
 
     // Perulangan WHILE
-    cout << "PERULANGAN WHILE" << endl;
+    cout <<"PERULANGAN WHILE" <<endl;
     x = 1 + rand() % 10;
 
     while (x <= 5) {
-        cout << "Bilangan acak = " << x << endl;
+        cout << "Bilangan acak = " <<x <<endl;
         x = rand() % 10;
     }
 
-    cout << "Bilangan acak while yang terakhir = " << x << endl;
-    cout << endl;
+    cout <<"Bilangan acak while yang terakhir =" <<x <<endl;
+    cout <<endl;
+
+     // Perulangan DO...WHILE
+    cout << "PERULANGAN DO...WHILE" <<endl;
+    x = 1 + rand() % 10;
+
+    do {
+        cout <<"Bilangan acak =" <<x <<endl;
+        x = rand() % 10;
+    } while (x <= 5);
+
+    cout << "Bilangan acak do-while yang terakhir =" <<x <<endl;
+    cout<<endl;
 }
