3e5b301addd0eb914a97f1a019d9c99997f8bace
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-30 19:14:51 +0700

    menambah output

--- switchCase.cpp
+++ switchCase.cpp
@@ -37,3 +37,11 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+// prosedur
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
