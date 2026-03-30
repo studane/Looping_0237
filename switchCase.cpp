3cfa949b0c74050a19c2d6c855d383826aad77a1
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-30 19:13:38 +0700

    menambahkan logika tiap jurusan

--- switchCase.cpp
+++ switchCase.cpp
@@ -18,5 +18,22 @@ void input() {
 }
 
 // prosedur untuk pengecekan kondisi
-void pilihanJurusan() 
-    switch (kodeJurusan)
+void pilihanJurusan() {
+    switch (kodeJurusan) {
+        case 1:
+            namaJurusan = "Teknologi Informasi";
+            statusKuota = "Tersedia (15 Kursi)";
+            break;
+        case 2:
+            namaJurusan = "Teknik Elektro";
+            statusKuota = "Kuota Penuh!";
+            break;
+        case 3:
+            namaJurusan = "Sistem Informasi";
+            statusKuota = "Tersedia (5 Kursi)";
+            break;
+        default:
+            namaJurusan = "Tidak Diketahui";
+            statusKuota = "Error: Kode Tidak Valid";
+    }
+}
